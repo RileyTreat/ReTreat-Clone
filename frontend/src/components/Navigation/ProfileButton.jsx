62946ee6158209002c30affbab6f9e8da73dae0c
RileyTreat <riley.treat@hotmail.com>
2024-10-11 01:18:27 -0400

    phase 5 complete without options put in. attempting css styling for profile button/ login modal, signup modal

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,8 +1,13 @@
+// frontend/src/components/Navigation/ProfileButton.jsx
+
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaHouseChimneyUser } from "react-icons/fa6";
 import * as sessionActions from '../../store/session';
-import './Navigation.css';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
+import './ProfileButton.css'
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -10,8 +15,7 @@ function ProfileButton({ user }) {
   const ulRef = useRef();
 
   const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-    // if (!showMenu) setShowMenu(true);
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
     setShowMenu(!showMenu);
   };
 
@@ -19,14 +23,14 @@ function ProfileButton({ user }) {
     if (!showMenu) return;
 
     const closeMenu = (e) => {
-      if (ulRef.current && !ulRef.current.contains(e.target)) {
+      if (!ulRef.current.contains(e.target)) {
         setShowMenu(false);
       }
     };
 
     document.addEventListener('click', closeMenu);
 
-    return () => document.removeEventListener('click', closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
   const logout = (e) => {
@@ -38,57 +42,38 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={toggleMenu}>
+      <button onClick={toggleMenu} className="menu-button">
       <FaHouseChimneyUser />
       </button>
       <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
       </ul>
     </>
   );
 }
 
 export default ProfileButton;
-
-
-
-// import { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { FaHouseChimneyUser } from "react-icons/fa6";
-// import * as sessionActions from '../../store/session';
-// import './Navigation.css';
-
-// function ProfileButton({ user }) {
-//   const dispatch = useDispatch();
-//   const [showMenu, setShowMenu] = useState(false);
-
-//   const logout = (e) => {
-//     e.preventDefault();
-//     dispatch(sessionActions.logout());
-//   };
-
-//   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-
-//   return (
-//     <>
-//       <button onClick={() => setShowMenu(!showMenu)}>
-//       <FaHouseChimneyUser />
-//       </button>
-//       <ul className={ulClassName}>
-//         <li>{user.username}</li>
-//         <li>{user.firstName} {user.lastName}</li>
-//         <li>{user.email}</li>
-//         <li>
-//           <button onClick={logout}>Log Out</button>
-//         </li>
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default ProfileButton;
